7241a8a7889c59b6b5b8649ff8b4c9cbb719c601
Harry Collins <sensorydog@gmail.com>
2024-08-17 19:55:38 +1000

    motion or keypress kills the process

--- main.py
+++ main.py
@@ -1,34 +1,46 @@
 import turtle
 import random
-import tkinter
+import tkinter as tk
 
-root = tkinter.Tk()
-root.withdraw()  # Hide the root window
+def close_window(event=None):
+    """Function to close the Turtle graphics window."""
+    turtle.bye()  # Close the turtle graphics window
 
-# Get screen resolution
-screen_width = root.winfo_screenwidth()
-screen_height = root.winfo_screenheight()
+def set_fullscreen():
+    """Function to set the window to fullscreen and remove border."""
+    screen = turtle.Screen()
+    tk_window = screen._root  # Get the Tkinter window object
+    tk_window.attributes('-fullscreen', True)  # Set fullscreen mode
+    tk_window.overrideredirect(True)  # Remove window border and title bar
+
+def setup_event_handlers():
+    """Function to set up event handlers for closing the window."""
+    screen = turtle.Screen()
+    tk_window = screen._root  # Get the Tkinter window object
+    
+    # Bind any key press and mouse movement to close the window
+    tk_window.bind("<KeyPress>", close_window)
+    tk_window.bind("<Motion>", close_window)
+
+# Set up the turtle screen
+set_fullscreen()  # Make the turtle window fullscreen and borderless
+
+screen = turtle.Screen()
+screen.bgcolor("black")  # Set background color
 
 # Set up the turtle
 turtle.hideturtle()
 turtle.speed(0)  # Fastest speed
 turtle.colormode(255)  # Use 0-255 range for colors
 turtle.pendown()
-turtle.bgcolor('black')
 
-def set_fullscreen():
-    # Access the underlying Tkinter window
-    screen = turtle.Screen()
-    tk_window = screen._root  # Get the Tkinter window object
-    tk_window.attributes('-fullscreen', True)  # Set fullscreen mode
-
-# Set up the turtle screen and background color
-set_fullscreen()  # Make the turtle window fullscreen
+# Set up event handlers
+setup_event_handlers()
 
 # Run the drawing loop
-while True:
+while True:  # Draw 1000 lines
     turtle.pencolor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-    turtle.pensize(random.randint(0, 10))
-    turtle.goto(random.randint(-screen_width, screen_width), random.randint(-screen_height, screen_height))
+    turtle.goto(random.randint(-1000, 1000), random.randint(-1000, 1000))
 
-turtle.done()  # Finish drawing
+# Finish drawing
+turtle.done()
